ea465d528209e81d8cb1f42bb7a49de1f65bb017
Leonid Kulikov <leokul01@students.programming1189.ru>
2017-10-14 20:27:30 +0300

    Made primitive crossover & function of defining survival coefficient & little supplements

--- main.cpp
+++ main.cpp
@@ -2,13 +2,55 @@
 #include <vector>
 #include <time.h>
 #define N 10
-#define mut_prob 0.2
+#define K 4
+#define MIN -100
+#define MAX 100
+#define MUT 0.2
 using namespace std;
+
+double func(double x1, double x2, double x3, double x4);
+void cross_prob(vector<double> &surv, int &p1, int &p2);
+void def_surv(vector<double> &surv, vector<vector<int>> &gen);
+double random(double min, double max);
+
+int main()
+{
+    srand(time(0));
+    vector<vector<double>> gen(N, vector<double>());
+    vector<double> surv(N);
+
+    // Creation of first generation
+    for(int i=0;i<N;i++)
+        for(int j=0;j<K;j++)
+            gen[i].push_back(random());
+
+    // Def of surv coef's
+    def_surv(surv, gen);
+
+    //Crossover
+    int p1, p2;
+    for(int i=0;i<N/2;i++)
+    {
+        cross_prob(surv, p1, p2);
+        swap(gen[p1][0],gen[p2][0]);
+        swap(gen[p1][1],gen[p2][1]);
+    }
+
+
+
+    return 0;
+}
 
 double func(double x1, double x2, double x3, double x4)
 {
-    return x1+2*x2+3*x3+4*x4;
+    return x1+2*x2+3*x3+4*x4-30;
 }
+
+double random()
+{
+    return (double)(rand())/RAND_MAX*(MAX - MIN) + MIN;
+}
+
 void cross_prob(vector<double> &surv, int &p1, int &p2)
 {
     vector<int> dist;
@@ -25,11 +67,14 @@ void cross_prob(vector<double> &surv, int &p1, int &p2)
     p2 = rand() % dist.size();
 }
 
-int main()
+void def_surv(vector<double> &surv, vector<vector<int>> &gen)
 {
-    srand(time(0));
-
-
-
-    return 0;
+    double sum = 0.0;
+    for(int i=0;i<N;i++) {
+        surv[i] = fabs(func(gen[i][0], gen[i][1], gen[i][2], gen[i][3]));
+        sum += surv[i];
+    }
+    sum = fabs(sum);
+    for(int i=0;i<N;i++)
+        surv[i] = surv[i]/sum;
 }
